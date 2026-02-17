c94b9486d3df6ff84f7959244644a15fc210ab2d
Vasily Efimov <vanys.efim@gmail.com>
2026-02-17 23:30:29 +0300

    hw0

---
+++ start/hw0/server.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"net"
+)
+
+func main() {
+	listener, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer listener.Close()
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		_, _ = conn.Write([]byte("OK\n"))
+		_ = conn.Close()
+	}
+}
